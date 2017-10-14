938a72d2364573cf38e33b8c44292e0646f577db
ChristofferOne <christofferlundberg.98@hotmail.com>
2017-10-14 12:13:43 +0200

    Removed unit tests for now since i updated the model to use CoreDate to store user information on disk

--- To Dooo/TodoTabBarController.swift
+++ To Dooo/TodoTabBarController.swift
@@ -7,33 +7,80 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoModel {
     private var pendingItems:[String] = []
     private var doneItems:[String] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     func addPending(text:String) {
-        self.pendingItems.append(text)
+        addToContext(entity: "Pending", text: text)
     }
     
     func addDone(text:String) {
-        self.doneItems.append(text)
+        addToContext(entity: "Done", text: text)
     }
     
     func removePending(index:Int) {
-        self.pendingItems.remove(at: index)
+        deleteInContext(index: index, entity: "Pending")
     }
     
     func removeDone(index:Int) {
-        self.doneItems.remove(at: index)
+        deleteInContext(index: index, entity: "Done")
     }
     
     func getPending() -> [String] {
-        return self.pendingItems
+        return retrieveContext(entity: "Pending")
     }
     
     func getDone() -> [String] {
-        return self.doneItems
+        return retrieveContext(entity: "Done")
+    }
+    
+    func addToContext(entity:String, text:String) {
+        let newPending = NSEntityDescription.insertNewObject(forEntityName: entity, into: context)
+        newPending.setValue(text, forKey: "text")
+        do {
+            try context.save()
+        } catch {
+            print("Saving error to handle")
+        }
+    }
+    
+    func retrieveContext(entity:String) -> [String] {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+        request.returnsObjectsAsFaults = false
+        var resultArray:[String] = []
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let text = result.value(forKey: "text") as? String {
+                        resultArray.append(text)
+                    }
+                }
+            }
+        } catch {
+            print("Failed to get request")
+        }
+        return resultArray
+    }
+    
+    func deleteInContext(index:Int, entity:String) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(request)
+            context.delete(results[index] as! NSManagedObject)
+            do {
+                try context.save()
+            } catch {
+                print("Saving error to handle")
+            }
+        } catch {
+            print("Failed to get request")
+        }
     }
 }
 
